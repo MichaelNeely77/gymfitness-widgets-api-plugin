c9453dd6d2097b1aa1292811e42a50144669829f
Michael Neely <michael@michaelneely.net>
2020-08-15 23:52:19 -0400

    Added more functions to the widget

--- gymfitness_widgets.php
+++ gymfitness_widgets.php
@@ -39,6 +39,8 @@ class GymFitness_Classes_Widget extends WP_Widget {
 	public function widget( $args, $instance ) {
 		echo $args['before_widget'];
 		?>
+		<h2 class="text-primary text-center classes-header">Our Classes</h2>
+
 		<ul class="sidebar-classes-list">
 			<?php
 				$args = array(
@@ -49,15 +51,20 @@ class GymFitness_Classes_Widget extends WP_Widget {
 				$classes = new WP_Query($args);
 				while($classes->have_posts()): $classes->the_post();
 			?>
-				<li class="sidebar_class">
+				<li class="sidebar-class">
 					<div class="sidebar-widget-image">
 						<?php the_post_thumbnail('thumbnail'); ?>
 
 					</div>
 					<div class="class-sidebar-content">
 						<a href="<?php the_permalink(); ?>">
-							<h3><?php the_title(); ?></h3>
+							<h3 class="text-primary sidebar-class-title"><?php the_title(); ?></h3>
 						</a>
+						<?php 
+							$start_time = get_field('start_time');
+							$end_time = get_field('end_time_');
+                		?>
+             		<p><?php echo the_field('class_days') . " - " . $start_time . " to "  . $end_time; ?></p>
 					</div>
 					
 				
